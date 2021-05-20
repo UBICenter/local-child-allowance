e80b78015b5b843e542f8b82fe4dee4b134810eb
Ben Boehlert <bhboehlert@gmail.com>
2021-05-20 16:20:12 -0400

    with csv export

--- ACS_child_allowance_rough_draft.py
+++ ACS_child_allowance_rough_draft.py
@@ -27,3 +27,48 @@ mdf.poverty_rate(just_children, "new_resources", "spm_povthreshold", "wt")
 mdf.poverty_rate(just_children, "spm_resources", "spm_povthreshold", "wt")
 
 # Columns for PUMA, child, adult, or all, deep or regular, baseline and reform
+
+
+def pov(data):
+    base = mdf.poverty_rate(data, "spm_resources", "spm_povthreshold")
+    reform = mdf.poverty_rate(data, "new_resources", "spm_povthreshold")
+    deep_base = mdf.deep_poverty_rate(data, "spm_resources", "spm_povthreshold")
+    deep_reform = mdf.deep_poverty_rate(data, "new_resources", "spm_povthreshold")
+    return pd.Series(
+        {
+            "poverty_base": base,
+            "poverty_reform": reform,
+            "deep_poverty_base": deep_base,
+            "deep_poverty_reform": deep_reform,
+        }
+    )
+
+
+grouped_puma_child = person3.groupby(["puma", "child"]).apply(pov)
+grouped_puma = person3.groupby(["puma"]).apply(pov)
+grouped_puma_child["pct_change"] = (
+    grouped_puma_child["poverty_base"] - grouped_puma_child["poverty_reform"]
+) / grouped_puma_child["poverty_base"]
+grouped_puma_child["pct_change_deep"] = (
+    grouped_puma_child["deep_poverty_base"] - grouped_puma_child["deep_poverty_reform"]
+) / grouped_puma_child["deep_poverty_base"]
+grouped_puma["pct_change"] = (
+    grouped_puma["poverty_base"] - grouped_puma["poverty_reform"]
+) / grouped_puma["poverty_base"]
+grouped_puma["pct_change_deep"] = (
+    grouped_puma["deep_poverty_base"] - grouped_puma["deep_poverty_reform"]
+) / grouped_puma["deep_poverty_base"]
+grouped_puma_child.groupby("child").mean()
+grouped_puma_child.groupby("child").std()
+pip install 
+grouped_list = [grouped_puma, grouped_puma_child]
+grouped_df = pd.concat(grouped_list)
+only_children_pumas = grouped_puma_child[(grouped_puma_child.child == 1)]
+grouped_df["pct_change"].mean()
+# TODO: Add another not grouped by child
+# Add pct change columns for poverty and deep poverty
+# Stack those
+
+# TODO:
+# - Export to csv (data/poverty.csv)
+# - import plotly.express as px; px.hist
