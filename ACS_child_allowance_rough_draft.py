a859349066b76fc00c3654c1cb757cbbfdee65e4
Ben Boehlert <bhboehlert@gmail.com>
2021-09-02 17:00:28 -0400

    maps, mapping files, policy simulation updated

--- ACS_child_allowance_rough_draft.py
+++ ACS_child_allowance_rough_draft.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
 import microdf as mdf
+from pandas.core.reshape.concat import concat
 
-person = pd.read_stata(
+person2018 = pd.read_stata(
     "spm_2018_pu.dta",
     columns=[
         "spm_id",
@@ -15,10 +16,39 @@ person = pd.read_stata(
         "wt",
     ],
 )
-
+person2018["year"] = 2018
+person2017 = pd.read_stata(
+    "spm_2017_pu.dta",
+    columns=[
+        "spm_id",
+        "puma",
+        "spm_povthreshold",
+        "spm_resources",
+        "age",
+        "spm_totval",
+        "st",
+        "wt",
+    ],
+)
+person2017["year"] = 2017
+person2016 = pd.read_stata(
+    "spm_2016_pu.dta",
+    columns=[
+        "spm_id",
+        "puma",
+        "spm_povthreshold",
+        "spm_resources",
+        "age",
+        "spm_totval",
+        "st",
+        "wt",
+    ],
+)
+person2016["year"] = 2016
+years_frame = [person2018, person2017, person2016]
+person = pd.concat(years_frame)
 # VAR LIST: 'spm_id' 'spm_povthreshold' 'spm_resources'
 #  'puma' 'age' 'st' 'spm_totval' 'wt'
-
 mapping_house = pd.read_csv(
     "Mapping1.csv",
     usecols=["Assembly District Number", "PUMA Description", "STATEFIP", "PUMA"],
@@ -26,28 +56,25 @@ mapping_house = pd.read_csv(
 mapping_senate = pd.read_csv(
     "Mapping2.csv", usecols=["Senate District Number", "PUMA", "STATEFIP"]
 )
-mapping_senate
-mapping_house
-mapping_senate["Senate District Number"].nunique()
-mapping_senate_CA = mapping_senate[(mapping_senate["STATEFIP"] == 6)]
-mapping_house_CA = mapping_house[(mapping_house["STATEFIP"] == 6)]
-mapping_merged_CA = mapping_house_CA.merge(
-    mapping_senate_CA[["Senate District Number", "PUMA"]], on=["PUMA"]
+
+mapping_merged = mapping_house.merge(
+    mapping_senate[["Senate District Number", "PUMA", "STATEFIP"]],
+    on=["STATEFIP", "PUMA"],
 )
-mapping_merged_CA = mapping_merged_CA.rename(columns={"PUMA": "puma"})
-mapping_merged_CA
+mapping_merged = mapping_merged.rename(columns={"PUMA": "puma"})
 
 person["child"] = np.where(person["age"] < 18, 1, 0)
-spmu = person.groupby(["spm_id", "spm_resources"])[["child"]].sum().reset_index()
+spmu = (
+    person.groupby(["spm_id", "spm_resources", "year"])[["child"]].sum().reset_index()
+)
 spmu.rename(columns={"child": "spmu_children"}, inplace=True)
 ANNUAL_CHILD_ALLOWANCE = 1200
 spmu["new_resources"] = spmu.spm_resources + ANNUAL_CHILD_ALLOWANCE * spmu.spmu_children
-person3 = person.merge(spmu[["new_resources", "spm_id"]], on=["spm_id"])
-mdf.poverty_rate(person3, "new_resources", "spm_povthreshold", "wt")
-mdf.poverty_rate(person3, "spm_resources", "spm_povthreshold", "wt")
+person3 = person.merge(spmu[["spm_id", "new_resources", "year"]], on=["year", "spm_id"])
+person3.new_resources.describe()
+person3.spm_resources.describe()
+
 just_children = person3[(person3.child == 1)]
-mdf.poverty_rate(just_children, "new_resources", "spm_povthreshold", "wt")
-mdf.poverty_rate(just_children, "spm_resources", "spm_povthreshold", "wt")
 # Columns for PUMA, child, adult, or all, deep or regular, baseline and reform
 
 
@@ -66,20 +93,22 @@ def pov(data):
     )
 
 
+def pct_chg(data):
+    return (data["poverty_base"] - data["poverty_reform"]) / data["poverty_base"]
+
+
+def pct_chg_deep(data):
+    return (data["deep_poverty_base"] - data["deep_poverty_reform"]) / data[
+        "deep_poverty_base"
+    ]
+
+
 grouped_puma_child = person3.groupby(["puma", "child"]).apply(pov).reset_index()
 grouped_puma = person3.groupby(["puma"]).apply(pov).reset_index()
-grouped_puma_child["pct_change"] = (
-    grouped_puma_child["poverty_base"] - grouped_puma_child["poverty_reform"]
-) / grouped_puma_child["poverty_base"]
-grouped_puma_child["pct_change_deep"] = (
-    grouped_puma_child["deep_poverty_base"] - grouped_puma_child["deep_poverty_reform"]
-) / grouped_puma_child["deep_poverty_base"]
-grouped_puma["pct_change"] = (
-    grouped_puma["poverty_base"] - grouped_puma["poverty_reform"]
-) / grouped_puma["poverty_base"]
-grouped_puma["pct_change_deep"] = (
-    grouped_puma_child["deep_poverty_base"] - grouped_puma_child["deep_poverty_reform"]
-) / grouped_puma_child["deep_poverty_base"]
+grouped_puma_child["pct_change"] = pct_chg(grouped_puma_child)
+grouped_puma_child["pct_change_deep"] = pct_chg_deep(grouped_puma_child)
+grouped_puma["pct_change"] = pct_chg(grouped_puma)
+grouped_puma["pct_change_deep"] = pct_chg_deep(grouped_puma)
 grouped_list = [grouped_puma, grouped_puma_child]
 grouped_df = pd.concat(grouped_list).reset_index()
 only_children_pumas = grouped_puma_child[(grouped_puma_child.child == 1)]
@@ -91,6 +120,8 @@ px.histogram(grouped_df["pct_change"])
 px.histogram(grouped_df["pct_change_deep"])
 
 # About 13% of PUMAs have no change in the poverty rate
+### Find pov by state
+
 
 ### Just California
 CA_FIPS = 6
@@ -98,12 +129,18 @@ just_california = person3[(person3.st == CA_FIPS)].reset_index()
 just_california_children = just_california[(just_california.child == 1)].reset_index()
 # by individual
 poverty_change_CA = pov(just_california)
+poverty_change_CA
 poverty_change_CA_children = pov(just_california_children)
-poverty_change_CA_children
+poverty_change_CA_pct = pct_chg(poverty_change_CA)
+poverty_change_CA_pct
+poverty_change_CA_children_pct = pct_chg(poverty_change_CA_children)
+poverty_change_CA_children_pct
 just_california["puma"] = just_california["puma"].astype(int)
 just_california["puma"] = just_california.puma - 600000
+just_california["puma"]
+mapping_CA = mapping_merged[(mapping_merged.STATEFIP == 6)]
 merged_individuals_CA = just_california.merge(
-    mapping_merged_CA[
+    mapping_CA[
         [
             "PUMA Description",
             "puma",
@@ -113,15 +150,9 @@ merged_individuals_CA = just_california.merge(
     ],
     on=["puma"],
 )
-pov(merged_individuals_CA)
-poverty_change_CA
-merged_individuals_CA.puma.nunique()
-just_california.puma.nunique()
 # Difference likely from four missing pumas
 pov_by_assembly = (
-    merged_individuals_CA.groupby(["Assembly District Number",])
-    .apply(pov)
-    .reset_index()
+    merged_individuals_CA.groupby(["Assembly District Number"]).apply(pov).reset_index()
 )
 pov_by_assembly_children = (
     merged_individuals_CA.groupby(["Assembly District Number", "child"])
@@ -136,8 +167,7 @@ pov_by_senate_children = (
     .apply(pov)
     .reset_index()
 )
-pov_by_assembly_children
-pov_by_senate_children
+pov_by_assembly
 pov_by_assembly["pct_change"] = (
     pov_by_assembly["poverty_base"] - pov_by_assembly["poverty_reform"]
 ) / pov_by_assembly["poverty_base"]
@@ -168,8 +198,31 @@ pov_by_senate_children["pct_change_deep"] = (
     pov_by_senate_children["deep_poverty_base"]
     - pov_by_senate_children["deep_poverty_reform"]
 ) / pov_by_senate_children["deep_poverty_base"]
-pov_by_senate_children = pov_by_senate_children[(pov_by_senate_children.child == 1)]
-pov_by_senate.to_csv(r"pov_by_senate_CA.csv")
+pov_by_senate_only_children = pov_by_senate_children[
+    (pov_by_senate_children.child == 1)
+]
+pov_by_assembly_only_children.columns = [
+    "child_" + i for i in pov_by_assembly_children.columns
+]
+pov_by_senate_only_children.columns = [
+    "child_" + i for i in pov_by_senate_only_children.columns
+]
+pov_by_senate_only_children.describe()
+pov_by_assembly_only_children.rename(
+    columns={"child_Assembly District Number": "Assembly District Number"}, inplace=True
+)
+pov_by_senate_only_children.rename(
+    columns={"child_Senate District Number": "Senate District Number"}, inplace=True
+)
+pov_by_senate_only_children.describe()
+pov_all_assembly = pov_by_assembly.merge(
+    pov_by_assembly_only_children, on="Assembly District Number"
+)
+pov_all_senate = pov_by_senate.merge(
+    pov_by_senate_only_children, on="Senate District Number"
+)
+
+
+pov_all_senate.to_csv(r"pov_all_senate_2.csv")
 pov_by_senate_children.to_csv(r"pov_by_senate_children_CA.csv")
-pov_by_assembly.to_csv(r"pov_by_assembly_CA.csv")
-pov_by_assembly_children.to_csv(r"pov_by_assembly_children_CA.csv")
+pov_all_assembly.to_csv(r"pov_all_assembly_2.csv")
